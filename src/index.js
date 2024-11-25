57d2e55d2f96c1afaa73ba9efa70f88a51cde244
Adwoa-Safowaah <adwoaadysne@gmail.com>
2024-11-25 12:05:32 +0000

    Added new files and updated changes in my javascript

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//alert("Hello world");
+//alert("My name is Adwoa Safowaah");
+
+let sardinebox = prompt("How many sardines do you want to buy?");
+alert(sardinebox + " It is!");
+alert("2" + "2")
+
+//variables naming conventions
+
+let friend = prompt ("What is your friend's name?");
+let age = prompt ("How old is your friend?");
+let favcolor = prompt ("What is the favorite color of your friend?"); 
+
+//alert("My beautiful friend's name is " + friend + ". She is " + age + " years old" + " and her favorite color is " + favcolor);
+alert(`My beautiful friend's name is ${friend}. She is ${age} years old and her favorite color is ${favcolor}.`);
+//My beautiful friend's name is ....................
+//She is .............. age years old and her favorite color is ..........
